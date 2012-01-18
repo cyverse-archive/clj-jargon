161f77a500db481d4ad7228eeeb749c94cbd6b72
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-01-18 09:24:39 -0700

    Initial commit of the code.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.iplantc/clj-jargon "0.1.0-SNAPSHOT"
+  :description "Clojure API on top of iRODS's jargon-core."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
+                 [org.irods.jargon/jargon-core "3.0.1-SNAPSHOT"]
+                 [org.irods.jargon/jargon-test "3.0.1-SNAPSHOT"]
+                 [org.irods.jargon/jargon-data-utils "3.0.1-SNAPSHOT"]
+                 [org.irods.jargon.transfer/jargon-transfer-engine "3.0.1-SNAPSHOT"]
+                 [org.irods.jargon/jargon-security "3.0.1-SNAPSHOT"]])
---
+++ test/clj_jargon/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-jargon.test.core
+  (:use [clj-jargon.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

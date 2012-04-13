e5538fb6fc000925c7236bd823d40932819c0fb1
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-04-13 10:39:36 -0700

    Added the repository information so maven/leiningen doesn't barf.

--- project.clj
+++ project.clj
@@ -6,4 +6,12 @@
                  [org.irods.jargon/jargon-test "3.0.1-SNAPSHOT"]
                  [org.irods.jargon/jargon-data-utils "3.0.1-SNAPSHOT"]
                  [org.irods.jargon.transfer/jargon-transfer-engine "3.0.1-SNAPSHOT"]
-                 [org.irods.jargon/jargon-security "3.0.1-SNAPSHOT"]])
+                 [org.irods.jargon/jargon-security "3.0.1-SNAPSHOT"]]
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"
+                 
+                 "renci.repository"
+                 "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"
+                 
+                 "sonatype"
+                 "http://oss.sonatype.org/content/repositories/releases"})

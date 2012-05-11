4f82cdf2d37ad18277b300a1e4718410a55561e4
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-05-11 08:49:37 -0700

    Bumped the version of jargon we're using.

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Clojure API on top of iRODS's jargon-core."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
-                 [org.irods.jargon/jargon-core "3.1.1-SNAPSHOT"]
-                 [org.irods.jargon/jargon-test "3.1.1-SNAPSHOT"]
-                 [org.irods.jargon/jargon-data-utils "3.1.1-SNAPSHOT"]
-                 [org.irods.jargon.transfer/jargon-transfer-engine "3.1.1-SNAPSHOT"]
+                 [org.irods.jargon/jargon-core "3.1.2-SNAPSHOT"]
+                 [org.irods.jargon/jargon-test "3.1.2-SNAPSHOT"]
+                 [org.irods.jargon/jargon-data-utils "3.1.2-SNAPSHOT"]
+                 [org.irods.jargon.transfer/jargon-transfer-engine "3.1.2-SNAPSHOT"]
                  [org.irods.jargon/jargon-security "3.1.2-SNAPSHOT"]]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"

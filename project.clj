10efdc2a59ccd15fabd98f676aa21c5e345e4bad
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-10-03 14:49:57 -0700

    Updated jargon to 3.2-beta1 and updated the version to 0.1.2.

--- project.clj
+++ project.clj
@@ -1,20 +1,23 @@
-(defproject org.iplantc/clj-jargon "0.1.1-SNAPSHOT"
+(defproject org.iplantc/clj-jargon "0.1.2-SNAPSHOT"
   :description "Clojure API on top of iRODS's jargon-core."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
-                 [org.irods.jargon/jargon-core "3.1.2"]
-                 [org.irods.jargon/jargon-data-utils "3.1.2"]
-                 [org.irods.jargon.transfer/jargon-transfer-engine "3.1.2"]
-                 [org.irods.jargon/jargon-security "3.1.2"]
+                 [org.irods.jargon/jargon-core "3.2.0-beta1"
+                  :exclusions [org.jglobus/JGlobus-Core]]
+                 [org.irods.jargon/jargon-data-utils "3.2.0-beta1"]
+                 [org.irods.jargon.transfer/jargon-transfer-engine "3.2.0-beta1"]
+                 [org.irods.jargon/jargon-security "3.2.0-beta1"]
+                 [org.irods.jargon/jargon-ticket "3.2.0-beta1"]
                  [org.clojure/tools.logging "0.2.3"]]
-  :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"
-                 
-                 "renci.repository.snapshots"
-                 "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"
-                 
-                 "renci.repository.releases"
-                 "http://ci-dev.renci.org/nexus/content/repositories/releases/"
-                 
-                 "sonatype"
-                 "http://oss.sonatype.org/content/repositories/releases"})
+  :repositories
+  {"iplantCollaborative"
+   "http://projects.iplantcollaborative.org/archiva/repository/internal/"
+   
+   "renci.repository.snapshots"
+   "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"
+   
+   "renci.repository.releases"
+   "http://ci-dev.renci.org/nexus/content/repositories/releases/"
+   
+   "sonatype"
+   "http://oss.sonatype.org/content/repositories/releases"})

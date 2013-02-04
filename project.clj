6e04c89a3cd83d5adc580ed01d75dbb8f13e4c17
Tony Edgin <tedgin@iplantcollaborative.org>
2013-02-04 14:43:43 -0700

    upgraded jargon to 3.2.1.3
    There is a bug fix that effects search.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Clojure API on top of iRODS's jargon-core."
   :dependencies [[org.clojure/clojure "1.4.0"]         
                  [org.iplantc/clojure-commons "1.3.0-SNAPSHOT"]
-                 [org.irods.jargon/jargon-core "3.2.0"
+                 [org.irods.jargon/jargon-core "3.2.1.3"
                   :exclusions [org.jglobus/JGlobus-Core]]
-                 [org.irods.jargon/jargon-data-utils "3.2.0"]
-                 [org.irods.jargon.transfer/jargon-transfer-engine "3.2.0"]
-                 [org.irods.jargon/jargon-security "3.2.0"]
-                 [org.irods.jargon/jargon-ticket "3.2.0"]
+                 [org.irods.jargon/jargon-data-utils "3.2.1.3"]
+                 [org.irods.jargon.transfer/jargon-transfer-engine "3.2.1.3"]
+                 [org.irods.jargon/jargon-security "3.2.1.3"]
+                 [org.irods.jargon/jargon-ticket "3.2.1.3"]
                  [slingshot "0.10.1"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[org.iplantc/boxy "0.1.0-SNAPSHOT"]]}}

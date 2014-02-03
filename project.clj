ea6936567af6f94212b9d75ce9b1ca1d17d53710
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-03 16:08:58 -0700

    CORE-5133: added some elements that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
-(defproject org.iplantc/clj-jargon "0.4.1"
+(defproject org.iplantc/clj-jargon "0.4.2-SNAPSHOT"
   :description "Clojure API on top of iRODS's jargon-core."
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/kameleon.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/kameleon.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.iplantc/clojure-commons "1.4.7"]
+                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.irods.jargon/jargon-core "3.3.1.1"
@@ -24,15 +33,11 @@
                  [slingshot "0.10.3"]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:dependencies [[org.iplantc/boxy "0.1.2-SNAPSHOT"]]}}
-  :repositories
-  {"iplantCollaborative"
-   "http://projects.iplantcollaborative.org/archiva/repository/internal/"
-
-   "renci.repository.releases"
-   "http://ci-dev.renci.org/nexus/content/repositories/releases/"
-
-   "renci.repository.snapshots"
-   "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"
-
-   "sonatype"
-   "http://oss.sonatype.org/content/repositories/releases"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]
+                 ["renci.repository.releases"
+                  {:url "http://ci-dev.renci.org/nexus/content/repositories/releases/"}]
+                 ["renci.repository.snapshots"
+                  {:url "http://ci-dev.renci.org/nexus/content/repositories/snapshots/"}]])
